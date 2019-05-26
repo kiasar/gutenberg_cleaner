5c621e9e7787da87eff169a1967b4318aea49f94
kiasari <mohsenikiasari@ce.sharif.edu>
2019-05-26 13:54:01 +0430

    new

--- _cleaning_options/cleaner.py
+++ _cleaning_options/cleaner.py
@@ -6,7 +6,7 @@ from _cleaning_options.strip_headers import _strip_headers
 def simple_cleaner(book: str) -> str:
     """
     Just removes lines that are part of the Project Gutenberg header or footer.
-    Doesnt go deeply in the text to remove other things like titles or footnotes or etc...
+    Doesnt go deeply in the text to remove other things like titles or footnotes or etc.
     :rtype: str
     :param book: str of a gutenberg's book
     :return: str of the book without the lines that are part of the Project Gutenberg header and footer.
